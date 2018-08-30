8f52257e716232ac4b8ea411ed86cedc3e44986a
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-08-30 04:31:35 +0300

    add featured url

--- main/urls.py
+++ main/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path("drink/", views.drink, name="drink"),
     path("vote/", views.vote, name="vote"),
     path("submit/", views.places_create, name="places_create"),
+    path("featured/", views.group_list, name="group_list"),
     path("list/", views.group_create, name="group_create"),
     path("list/<slug:route>/", views.group, name="group"),
 ]
